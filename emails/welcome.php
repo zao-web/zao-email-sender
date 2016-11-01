d571548d584b99197d6d00b7e949bc1fce4573ba
Liz Karaffa <liz@zao.is>
2016-11-01 11:03:01 -0700

    renamed file to let ajax work

---
+++ emails/welcome.php
@@ -0,0 +1,51 @@
+
+	<div class="pre-header" style="background-color: #eee;color: #eee;font-size: 5px;">
+		Welcome to the beginning of good things! We&#39;re so glad to be working with you and we&#39;re excited to get this project kicked into high gear.
+	</div>
+	<div class="email-background" style="background-color: #eee;padding: 15px;font-family: 'Helvetica Neue', Helvetica, sans-serif;">
+		<div class="email-container" style="max-width: 720px;background-color: #fff;margin: 0 auto;border-radius: 5px;padding: 10px;">
+			<div class="header" style="text-align: center;margin-top: 15px;">
+				<a href="http://zao.is" style="color: #4e8dca;text-decoration: none;">
+					<img src="http://zao.is/logo.png">
+				</a>
+				<h1 style="color: #4a545a;font-weight: 600;">Welcome to Awesome</h1>
+			</div>
+			<p style="margin: 20px;font-size: 18px;font-weight: 300px;color: #4a545a;line-height: 1.5;">Hello {first_name}!</p>
+
+			<p style="margin: 20px;font-size: 18px;font-weight: 300px;color: #4a545a;line-height: 1.5;">We&#39;re so glad to be working with you and we&#39;re excited to get this project kicked into high gear.  {custom_message}  You will or already have received a couple invitations for Slack and Breeze in your email.  These are private and confidential tools we use to keep our projects on time, clear, and fully communicated.</p>
+
+			<p style="margin: 20px;font-size: 18px;font-weight: 300px;color: #4a545a;line-height: 1.5;"><strong>Slack</strong> is a messaging app where we can talk, share files and work together. Having everything in one place will help us work together better and faster, rather than jumping around between emails, IMs, texts and a bunch of other programs. Everything you share in Slack is automatically indexed and archived, creating a searchable archive of all of our work. We want to be using the best communication tools to help us be more productive and make our lives easier. <a href="https://get.slack.help/hc/en-us/articles/218080037-Getting-started-for-new-users" style="color: #4e8dca;text-decoration: none;">Here is a resource provided by Slack</a> if you want more information on how to use it.</p> 
+
+			<p style="margin: 20px;font-size: 18px;font-weight: 300px;color: #4a545a;line-height: 1.5;">A few tips for slack:
+			<ul style="margin: 20px;font-size: 18px;font-weight: 300px;color: #4a545a;line-height: 1.5;">
+				<li>When talking to someone in the channel use <strong>@username</strong> so it&#39;s clear who you&#39;re talking to.  You can also use <strong>@channel</strong> if want everyone to know something.</li>
+				<li>You can <strong>star</strong> anything you feel is important to find it easier again in the future.</li>  
+				<li>You can easily <strong>drag and drop</strong> files into slack to make them available to the whole team.</li>
+				<li>Does an emoji not cut it? Type <strong>/giphy your random text</strong> for a fun surprise!</li>
+			</ul></p>
+
+			<p style="margin: 20px;font-size: 18px;font-weight: 300px;color: #4a545a;line-height: 1.5;"><strong>Breeze</strong> is a Kanban style board we use to keep all of our tasks for the project documented. With a quick glance you&#39;ll able to get a progress report on the current status of the project by seeing what needs doing, is in progress, and is completed.  Additionally, you&#39;ll be able to see what tasks we&#39;re waiting on you to complete in the <strong>Client to Do</strong> column.  Once you finish a task, drag and drop the task into the <strong>Client Done</strong> and we&#39;ll automatically get a notification in our Slack channel of what you&#39;ve done.</p>
+
+			<p style="margin: 20px;font-size: 18px;font-weight: 300px;color: #4a545a;line-height: 1.5;">If you click on individual tasks you will be able to see more information about that task including any descriptions, to do lists, or time estimates for a particular task. You&#39;ll also notice a <strong>Discussion</strong> section at the bottom of a task.  This is a great place to add an questions, comments, or bugs you have with a particular task we&#39;re working on as sometimes trying to reference what you&#39;re talking about in Slack may get a little too removed.  These comments will automatically get pushed to Slack, so at the end of the day, all the communication will still funnel to one place. <a href="http://www.breeze.pm/tour" style="color: #4e8dca;text-decoration: none;">Here is a tour of breeze</a> you can check out for more information.</p>
+
+			<p style="margin: 20px;font-size: 18px;font-weight: 300px;color: #4a545a;line-height: 1.5;">A few tips for Breeze:
+			<ul style="margin: 20px;font-size: 18px;font-weight: 300px;color: #4a545a;line-height: 1.5;">
+				<li>Pay attention to your <strong>Client to Do</strong> column to ensure we don&#39;t get backlogged waiting on portions from you.</li>
+				<li>Pay attention to the <strong>Zao Tested</strong> column. Items in this column are completed by us and we are waiting for you to sign off on them.</li>
+				<li>When a task is completed to your satisfaction move it to the <strong>Client Signed Off</strong> column</li>
+				<li>When commenting in a task, use <strong>@username</strong> just like in slack to communicate with the appropriate person.</li>
+			</ul></p>
+
+			<p style="margin: 20px;font-size: 18px;font-weight: 300px;color: #4a545a;line-height: 1.5;">Are you mobile frequently and want to keep up to date? Both Slack and Breeze have free apps for <strong>Android</strong> ( <a href="https://play.google.com/store/apps/details?id=com.Slack" style="color: #4e8dca;text-decoration: none;">Slack</a> | <a href="https://play.google.com/store/apps/details?id=pm.breeze.mobile" style="color: #4e8dca;text-decoration: none;">Breeze</a> ) and <strong>iOS</strong> ( <a href="https://itunes.apple.com/us/app/slack-team-communication/id618783545?mt=8" style="color: #4e8dca;text-decoration: none;">Slack</a> | <a href="https://itunes.apple.com/us/app/breeze-project-management/id1062974186" style="color: #4e8dca;text-decoration: none;">Breeze</a> ). Additionally Slack has a <a href="https://slack.com/downloads" style="color: #4e8dca;text-decoration: none;">desktop app</a> you can install too if you want.  These are all optional, you only need a browser if you don&#39;t like apps!</p>
+
+			<p style="margin: 20px;font-size: 18px;font-weight: 300px;color: #4a545a;line-height: 1.5;">Please don&#39;t hesitate to reach out with any questions.  We want this project to be as painless, seamless and enjoyable for you as possible.</p>
+
+			<p style="margin: 20px;font-size: 18px;font-weight: 300px;color: #4a545a;line-height: 1.5;">Thanks,</p>
+
+			<p style="margin: 20px;font-size: 18px;font-weight: 300px;color: #4a545a;line-height: 1.5;">Zao Team</p>
+		</div>
+		<div class="footer" style="background: none;padding: 20px;font-size: 11px;text-align: center;">
+			<a href="http://zao.is" style="color: #4e8dca;text-decoration: none;">zao.is</a> | <a href="mailto:office@zao.is" style="color: #4e8dca;text-decoration: none;">office@zao.is</a>
+		</div>
+		
+	</div>
